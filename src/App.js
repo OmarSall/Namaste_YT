810ffce699e4f241f11a44fe7e6998934e7e7dd8
Omar Salloum <omarsalloum08@gmail.com>
2026-05-09 19:42:08 +0200

    adding redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Head from "./components/Head";
 import Body from "./components/Body";
+import {Provider} from "react-redux";
+import store from "./utils/store";
 
 function App() {
     return (
-        <div className="">
-            <Head/>
-            <Body/>
-            {/*
+        <Provider store={store}>
+            <div className="">
+                <Head/>
+                <Body/>
+                {/*
     Head
     Body
         SideBar
@@ -17,7 +20,8 @@ function App() {
             VideoContainer
                 VideoCard
     */}
-        </div>
+            </div>
+        </Provider>
     );
 }
 
